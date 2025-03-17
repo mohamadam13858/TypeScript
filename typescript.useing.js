00302c4be7b120e8ececb2ad7c0c22c7b48e759a
mohamad <mh711748@gmail.com>
2025-03-17 18:15:50 +0330

    array1

--- typescript.useing.js
+++ typescript.useing.js
@@ -24,13 +24,23 @@ var person = {
     isMale: true,
     hobbies: [
         "sports", "cooking", "watch Movies"
-    ]
+    ],
+    role: [11, "nn"]
 };
+person.role.push("admin");
+console.log(person);
 var favoriteActivities;
 favoriteActivities = ['music', 'coding'];
 // console.log(person.name + "-" + person.age);
-console.log(person.name);
+// console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
 }
+var arrayExample = ['mohamad', 32, true];
+arrayExample[0] = false;
+arrayExample[1] = "hassan";
+var tupleExample;
+tupleExample = ["mj", 55, true];
+tupleExample[0] = "jdjh";
+tupleExample[1] = 55;
--- typescript.useing.ts
+++ typescript.useing.ts
@@ -39,14 +39,26 @@ add(number1, number2, isPrintResult, resultText);
 
 // let person:object;
 
-let person = {
+let person:{
+    name:string,
+    age:number,
+    isMale:boolean,
+    hobbies:string[],
+    role: [number , string]
+} = {
     name: "mohamad",
     age: 23,
     isMale: true,
     hobbies: [
         "sports", "cooking", "watch Movies"
-    ]
+    ],
+    role:[11 , "nn"]
 }
+
+person.role.push("admin")
+
+console.log(person);
+
 
 
 let favoriteActivities: string[];
@@ -56,13 +68,28 @@ favoriteActivities = ['music', 'coding'];
 
 // console.log(person.name + "-" + person.age);
 
-console.log(person.name);
+// console.log(person.name);
 
 
 for (const hobby of person.hobbies) {
     console.log(hobby);
 
 }
+
+
+let arrayExample = ['mohamad', 32, true]
+
+arrayExample[0] = false
+arrayExample[1] = "hassan"
+
+
+
+let tupleExample: [string, number, boolean]
+
+tupleExample = ["mj", 55, true]
+
+tupleExample[0] = "jdjh"
+tupleExample[1] = 55
 
 
 
