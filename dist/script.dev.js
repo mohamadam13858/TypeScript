7e23eb834afe0652e2570983a4d4d6ce101cad88
mohamad <mh711748@gmail.com>
2025-03-19 20:22:27 +0330

    class User

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,5 +1,12 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log("clicked !!!");
-});
+class User {
+    constructor(e, pn, pass) {
+        this.email = e;
+        this.phonenumber = pn;
+        this.password = pass;
+    }
+}
+const firstUser = new User("mohamadhiohe", 664644, "djijggjd;ksj");
+const secondUser = new User("kjkjj", 85585, "dlhjfhf");
+console.log(firstUser);
+console.log(secondUser);
--- src/script.dev.ts
+++ src/script.dev.ts
@@ -1,6 +1,21 @@
-const button = document.querySelector('button')!;
+class User {
+    email: string;
+    phonenumber: number;
+    password: string;
 
-button.addEventListener('click' , ()=>{
-    console.log("clicked !!!");
-    
-})
+    constructor(e: string, pn: number, pass: string) {
+        this.email = e;
+        this.phonenumber = pn;
+        this.password = pass;
+    }
+
+}
+
+const firstUser= new User("mohamadhiohe" , 664644 , "djijggjd;ksj");
+
+const secondUser = new User("kjkjj" , 85585 , "dlhjfhf")
+
+console.log(firstUser);
+console.log(secondUser);
+
+
