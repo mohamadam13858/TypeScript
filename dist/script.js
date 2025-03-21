1b5b1b85dd4f0ca7af798ca7cb3ef9b96d61a133
mohamad <mh711748@gmail.com>
2025-03-21 13:44:18 +0330

    if-in2

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('fjfj');
+const e1 = {
+    name: "mohamad",
+    phoneNumber: 9138949241,
+    startDate: new Date()
+};
+function addParamter(a, b) {
+    if (typeof a == "number" && typeof b == "number") {
+        return a + b;
+    }
+}
+function printEmployeeInfo(emp) {
+    console.log('name' + emp.name);
+    if ('startDate' in emp) {
+        console.log('start date is' + emp.startDate);
+    }
+    if ("phoneNumber" in emp) {
+        console.log(emp.phoneNumber);
+    }
+}
