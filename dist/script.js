8edbc82f56e606fce9d6bde4661b206c83de8110
mohamad <mh711748@gmail.com>
2025-03-24 21:34:19 +0330

    genericfunctions3

--- dist/script.js
+++ dist/script.js
@@ -7,15 +7,15 @@
 //        description: "jhfihjjhfi"
 //     }
 // }
-// console.log(fetchUserData?.job?.description);
-// const Nemes: Array<string> = [
-// ]
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('this is Done')
-//     }, 2000);
-// });
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
-}
-const mergedObj2 = merge({ age: 55 }, { name: "mohamad" });
+const countAndDescribe = (element) => {
+    let descriptionText = 'Got No Value';
+    if (element.length === 1) {
+        descriptionText = "got 1 value ";
+    }
+    else if (element.length > 1) {
+        descriptionText = `got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+};
+console.log(countAndDescribe('hi mohamod'));
+console.log(countAndDescribe(['mohamad', 'leyla']));
--- src/script.ts
+++ src/script.ts
@@ -30,13 +30,34 @@
 
 
 
-function merge<T extends object, B extends object>(objA: T, objB: B) {
-    return Object.assign(objA, objB)
+// function merge<T extends object, B extends object>(objA: T, objB: B) {
+//     return Object.assign(objA, objB)
+// }
+
+
+
+// const mergedObj2 = merge({ age: 55 }, { name: "mohamad" });
+
+interface Lengthy {
+    length: number;
 }
 
 
 
-const mergedObj2 = merge({ age: 55 }, { name: "mohamad" });
+const  countAndDescribe =<T extends Lengthy>(element: T) => {
+    let descriptionText = 'Got No Value' ;
+      if (element.length === 1 ) {
+        descriptionText = "got 1 value "
+      }else if (element.length > 1) {
+        descriptionText = `got ${element.length} elements`
+      }
+
+      return[element , descriptionText]
+}
+
+
+console.log(countAndDescribe('hi mohamod'));
+console.log(countAndDescribe(['mohamad' , 'leyla']));
 
 
 
