63cdece4dfe1d339411e96743880270dbab86752
mohamad <mh711748@gmail.com>
2025-03-26 13:06:03 +0330

    createautobind

--- dist/script.js
+++ dist/script.js
@@ -1,90 +1,106 @@
 "use strict";
+// const Logger = (logString: string) => {
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-const Logger = (logString) => {
-    return function (constructor) {
-        console.log(constructor);
-        console.log(logString);
-    };
-};
-const addTemplate = (template, hookId) => {
-    return function (constructor) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1').textContent = p.name;
+//     return function (constructor: Function) {
+//         console.log(constructor);
+//         console.log(logString);
+//     }
+// }
+// const addTemplate = (template: string, hookId: string) => {
+//     return function (constructor: any) {
+//         const hookEl = document.getElementById(hookId)
+//         const p = new constructor();
+//         if (hookEl) {
+//             hookEl.innerHTML = template;
+//             hookEl.querySelector('h1')!.textContent = p.name
+//         }
+//     }
+// }
+// // @Logger("LOGGING - PERSON - LINE 11")
+// @addTemplate('<h1> My Person Object</h1>', 'app')
+// class Personn {
+//     name = "mohamad"
+//     constructor() {
+//         console.log("creating person object ... ");
+//     }
+// }
+// const personn = new Personn();
+// // console.log(personn);
+// // -------------
+// const Log = (target: any, propertyName: any) => {
+//     console.log("log Decorator Excuted");
+//     console.log(target);
+//     console.log(propertyName);
+// }
+// const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
+//     console.log("accessor decorator");
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+// const Log3 = (target: any , name: string , descriptor: PropertyDescriptor) => {
+//     console.log("jfjfjfj decorator");
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+// const Log4 = (target:any , name:string , position: number) => {
+//     console.log("mmmmmmm decorator");
+//     console.log(target);
+//     console.log(name);
+//     console.log(position);
+// }
+// class Product {
+//     @Log
+//     title: string;
+//     private _price: number;
+//     @Log2
+//     set price(val: number) {
+//         if (val > 0) {
+//             this._price = val
+//         }
+//         else {
+//             throw Error("Invalid Price")
+//         }
+//     }
+//     constructor(t: string, p: number) {
+//         this.title = t,
+//             this._price = p
+//     }
+//     @Log3
+//     getPriceWithTax( tax: number ,@Log4 tax2:number) {
+//         return this._price + tax;
+//     }
+// }
+// const p = new Product("lap top" , 12000)
+const Autobind = (target, name, descriptor) => {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const buundfn = originalMethod.bind(this);
+            return buundfn;
         }
     };
+    return adjDescriptor;
 };
-// @Logger("LOGGING - PERSON - LINE 11")
-let Personn = class Personn {
+class Printer {
     constructor() {
-        this.name = "mohamad";
-        console.log("creating person object ... ");
+        this.message = "this works!";
     }
-};
-Personn = __decorate([
-    addTemplate('<h1> My Person Object</h1>', 'app')
-], Personn);
-const personn = new Personn();
-// console.log(personn);
-// -------------
-const Log = (target, propertyName) => {
-    console.log("log Decorator Excuted");
-    console.log(target);
-    console.log(propertyName);
-};
-const Log2 = (target, name, descriptor) => {
-    console.log("accessor decorator");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-};
-const Log3 = (target, name, descriptor) => {
-    console.log("jfjfjfj decorator");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-};
-const Log4 = (target, name, position) => {
-    console.log("mmmmmmm decorator");
-    console.log(target);
-    console.log(name);
-    console.log(position);
-};
-class Product {
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw Error("Invalid Price");
-        }
-    }
-    constructor(t, p) {
-        this.title = t,
-            this._price = p;
-    }
-    getPriceWithTax(tax, tax2) {
-        return this._price + tax;
+    showMessage() {
+        alert(this.message);
     }
 }
 __decorate([
-    Log
-], Product.prototype, "title", void 0);
-__decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(1, Log4)
-], Product.prototype, "getPriceWithTax", null);
-const p = new Product("lap top", 12000);
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector('button');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', p.showMessage);
--- src/script.ts
+++ src/script.ts
@@ -1,103 +1,139 @@
-const Logger = (logString: string) => {
+// const Logger = (logString: string) => {
 
-    return function (constructor: Function) {
-        console.log(constructor);
-        console.log(logString);
+//     return function (constructor: Function) {
+//         console.log(constructor);
+//         console.log(logString);
 
-    }
-}
+//     }
+// }
 
 
-const addTemplate = (template: string, hookId: string) => {
-    return function (constructor: any) {
-        const hookEl = document.getElementById(hookId)
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name
+// const addTemplate = (template: string, hookId: string) => {
+//     return function (constructor: any) {
+//         const hookEl = document.getElementById(hookId)
+//         const p = new constructor();
+//         if (hookEl) {
+//             hookEl.innerHTML = template;
+//             hookEl.querySelector('h1')!.textContent = p.name
+//         }
+//     }
+// }
+
+// // @Logger("LOGGING - PERSON - LINE 11")
+
+
+// @addTemplate('<h1> My Person Object</h1>', 'app')
+// class Personn {
+//     name = "mohamad"
+
+//     constructor() {
+//         console.log("creating person object ... ");
+
+//     }
+
+
+// }
+
+// const personn = new Personn();
+
+
+
+// // console.log(personn);
+
+
+// // -------------
+
+
+// const Log = (target: any, propertyName: any) => {
+//     console.log("log Decorator Excuted");
+//     console.log(target);
+//     console.log(propertyName);
+
+
+// }
+
+// const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
+//     console.log("accessor decorator");
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+
+// const Log3 = (target: any , name: string , descriptor: PropertyDescriptor) => {
+//     console.log("jfjfjfj decorator");
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+
+// const Log4 = (target:any , name:string , position: number) => {
+//     console.log("mmmmmmm decorator");
+//     console.log(target);
+//     console.log(name);
+//     console.log(position);
+// }
+
+
+// class Product {
+//     @Log
+//     title: string;
+//     private _price: number;
+//     @Log2
+//     set price(val: number) {
+//         if (val > 0) {
+//             this._price = val
+//         }
+//         else {
+//             throw Error("Invalid Price")
+//         }
+//     }
+//     constructor(t: string, p: number) {
+//         this.title = t,
+//             this._price = p
+//     }
+
+//     @Log3
+//     getPriceWithTax( tax: number ,@Log4 tax2:number) {
+//         return this._price + tax;
+
+//     }
+// }
+
+
+// const p = new Product("lap top" , 12000)
+
+
+const Autobind = (target: any, name: string, descriptor: PropertyDescriptor) => {
+
+    const originalMethod = descriptor.value;
+
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const buundfn = originalMethod.bind(this)
+
+            return buundfn;
         }
-    }
-}
-
-// @Logger("LOGGING - PERSON - LINE 11")
-
-
-@addTemplate('<h1> My Person Object</h1>', 'app')
-class Personn {
-    name = "mohamad"
-
-    constructor() {
-        console.log("creating person object ... ");
 
     }
-
-
-}
-
-const personn = new Personn();
-
-
-
-// console.log(personn);
-
-
-// -------------
-
-
-const Log = (target: any, propertyName: any) => {
-    console.log("log Decorator Excuted");
-    console.log(target);
-    console.log(propertyName);
-
+    return adjDescriptor;
 
 }
 
-const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
-    console.log("accessor decorator");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
 
-const Log3 = (target: any , name: string , descriptor: PropertyDescriptor) => {
-    console.log("jfjfjfj decorator");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
+class Printer {
+    message = "this works!"
 
-const Log4 = (target:any , name:string , position: number) => {
-    console.log("mmmmmmm decorator");
-    console.log(target);
-    console.log(name);
-    console.log(position);
-}
-
-
-class Product {
-    @Log
-    title: string;
-    private _price: number;
-    @Log2
-    set price(val: number) {
-        if (val > 0) {
-            this._price = val
-        }
-        else {
-            throw Error("Invalid Price")
-        }
-    }
-    constructor(t: string, p: number) {
-        this.title = t,
-            this._price = p
-    }
-
-    @Log3
-    getPriceWithTax( tax: number ,@Log4 tax2:number) {
-        return this._price + tax;
-
+    @Autobind
+    showMessage() {
+        alert(this.message);
     }
 }
 
 
-const p = new Product("lap top" , 12000)
+const p = new Printer();
+
+const button = document.querySelector('button');
+
+button?.addEventListener('click', p.showMessage)
